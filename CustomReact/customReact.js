9e480725957a6c3a2cf0552ab74b52358687d8d9
Habib Tariq <157604419+habibtariq513@users.noreply.github.com>
2026-08-07 06:30:40 +0000

    feat: implement custom rendering of a clickable link in customReact.js

--- CustomReact/customReact.js
+++ CustomReact/customReact.js
@@ -1,1 +1,20 @@
-document.querySelector()
+cus
+
+const reactElement = {
+    type: 'a',
+    props: {
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    children: 'Click me to visit google'
+}
+
+const mainContainer = document.querySelector('#root')
+
+// Rendering: Addition of an Element with a Root.
+// Render function demands two things:
+//   - what to render
+//   - where to render
+
+// RenderFunc(what to render, where to render)
+customRender(reactElement, mainContainer)
